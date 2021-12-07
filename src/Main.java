81b7fc162dfb6d08c24a5773d7f72d2f5ac00d0c
Pedro Pedrosa <pmspedrosa@gmail.com>
2021-12-07 11:01:36 +0000

    fsdsdc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá");
+        System.out.println("Olá meus putos, daqui fala o Rick Fazeres");
     }
 }
