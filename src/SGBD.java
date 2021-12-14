8e6603b3d92d0aa736265713427028f2da6953bd
Pedro Pedrosa <pmspedrosa@gmail.com>
2021-12-14 12:16:55 +0000

    Changes by rpedr

--- src/SGBD.java
+++ src/SGBD.java
@@ -7,7 +7,7 @@ public class SGBD {
 
 
 
-//responsáFase1vel pela gestão e persistência da informação de suporte ao
+//responsável pela gestão e persistência da informação de suporte ao
 // sistema (contas de utilizadores, estado de ligação dos utilizadores,
 // histórico de mensagens trocadas e notificações de ficheiros disponibilizados,
 // grupos, contactos estabelecidos, etc).
